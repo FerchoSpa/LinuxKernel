6c44b241006ae33744a194dc1e9a1d9d137c22f8
Fernando Spadaro <rfspadaro.gmail.com>
2015-10-10 08:25:07 -0700

    Module loads

--- hellow2/hellow2.c
+++ hellow2/hellow2.c
@@ -1,8 +1,23 @@
 #include <linux/init.h>
+#include <linux/string.h>
 #include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/in.h>
+#include <linux/errqueue.h>
+
+MODULE_LICENSE("GPL") ;
+MODULE_AUTHOR("STUDENT") ;
+MODULE_AUTHOR("STUDENT") ;
+MODULE_DESCRIPTION("Printing Process Info") ;
 
 int sysint_init(void) {
+  struct task_struct* cur_task;
+
   printk("\n\nHello World, Module loaded \n\n\n");
+
+  cur_task = get_current();
+
+  printk (KERN_ALERT "\n\n STARTING WITH PROCESS : %s[%d]\r\n", cur_task->comm, cur_task->pid);
   return 0;
 }
 
